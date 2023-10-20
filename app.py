ec5c35a65845e16ce072109b93b4ba5979075a35
salomon <mtzelisalomon@gmail.com>
2023-10-19 18:17:12 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.static_folder = 'static'
+app.debug = True
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/compra')
+def compra():
+    return render_template('compra.html')
+
+if __name__ == '__main__':
+    app.run()
