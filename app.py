b67f520cdf4a842c5b8a14156366d92804a0df81
salomon <mtzelisalomon@gmail.com>
2023-10-19 18:27:26 -0600

    first commit

--- app.py
+++ app.py
@@ -14,3 +14,14 @@ def compra():
 
 if __name__ == '__main__':
     app.run()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/compra')
+def compra():
+    return render_template('compra.html')
+
+if __name__ == '__main__':
+    app.run()
